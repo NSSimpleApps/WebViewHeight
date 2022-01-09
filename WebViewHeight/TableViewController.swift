026568cfebf6634755d3d753d9b7dfd1115ad39b
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2022-01-10 00:00:21 +0700

    Обновление

--- WebViewHeight/TableViewController.swift
+++ WebViewHeight/TableViewController.swift
@@ -11,21 +11,14 @@ import WebKit
 
 class TableViewController: UITableViewController {
 
-    fileprivate var uiWebViewHeight: CGFloat = 100
-    fileprivate var wkWebViewHeight: CGFloat = 100
+    private var uiWebViewHeight: CGFloat = 100
+    private var wkWebViewHeight: CGFloat = 100
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = "Some cell"
 
@@ -33,9 +26,7 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
         let url = Bundle.main.url(forResource: "doc", withExtension: "html")
-        
         let webView = UIWebView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: self.uiWebViewHeight))
         webView.delegate = self
         webView.backgroundColor = UIColor.white
@@ -45,14 +36,11 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        
         return self.uiWebViewHeight
     }
     
     override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        
         let url = Bundle.main.url(forResource: "doc", withExtension: "html")!
-        
         let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: self.wkWebViewHeight))
         try! webView.loadFile(from: url)
         webView.navigationDelegate = self
@@ -61,34 +49,25 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        
         return self.wkWebViewHeight
     }
 }
 
 extension TableViewController: UIWebViewDelegate {
-    
     func webViewDidFinishLoad(_ webView: UIWebView) {
-        
         webView.scrollView.isScrollEnabled = false
         
         self.uiWebViewHeight = webView.documentHeight
-        
         self.tableView.beginUpdates()
         self.tableView.endUpdates()
     }
 }
 
 extension TableViewController: WKNavigationDelegate {
-    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        
         webView.scrollView.isScrollEnabled = false
-        
         webView.documentHeight { (height) in
-            
             self.wkWebViewHeight = height
-            
             self.tableView.beginUpdates()
             self.tableView.endUpdates()
         }
